a2e9f31d0900acbf00bdc7e193c998bcc1d06b20
Vanessa Santana <vanessa_santanatoledo@outlook.com>
2022-08-09 17:55:58 -0300

    fix: menu mobile e links

--- assets_dev/js/index.js
+++ assets_dev/js/index.js
@@ -1,1 +1,14 @@
-console.log("teste");
+const menu_hamburguer = document.querySelector('.icon-mobile');
+const menu = document.querySelector('.header-menu__mobile');
+
+menu_hamburguer.addEventListener('click', () => {
+  const active = menu.classList.toggle('header-menu__mobile--active');
+  const icon  = menu_hamburguer.querySelector('i');
+  if (!active) {
+    icon.classList.remove('fa-xmark');
+    icon.classList.add('fa-bars');
+    return;
+  }
+  icon.classList.remove('fa-bars');
+  icon.classList.add('fa-xmark');
+});
