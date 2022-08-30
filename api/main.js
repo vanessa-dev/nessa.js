1944a7fe85ffa799c6806f109a218498ebadf375
Vanessa Santana <vanessa_santanatoledo@outlook.com>
2022-08-29 21:12:57 -0300

    refactor: trocando a forma de enviar email para o nodemailer

---
+++ api/main.js
@@ -0,0 +1,52 @@
+require('dotenv/config');
+const express = require('express');
+const  cors = require('cors')
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const app = express();
+
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
+const email_destino = process.env.EMAIL_DESTINO;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post("/send-email", async (req, res) => {
+  const { name, email, surname, telephone, message } = req.body;
+  const html = `Nome: ${name}</br>. Sobrenome: ${surname || 'Não definido'}</br>. Email: ${email}</br>. Telefone: ${telephone || 'Não definido'}</br>. Messagem: ${message}</br>`;
+
+  const transport = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    auth: {
+      user: username,
+      pass: password
+    }
+  });
+
+  const message = {
+    from: username,
+    to: email_destino,
+    subject: "Mensagem do Website Nessa.js",
+    text: 'O(a) ${email} te enviou uma mensagem!',
+    html: 'teste'
+  };
+
+  transport.sendMail(message, function (err) {
+    if (err) return res.status(400).json({
+      erro: true,
+      mensagem: "Erro: E-mail não enviado com sucesso!"
+    });
+  });
+
+  return res.json({
+    erro: false,
+    mensagem: "E-mail enviado com sucesso!"
+  });
+
+});
+
+app.listen(3000, () => {
+  console.log("Servidor iniciado na porta 3000 http://localhost:3000");
+});
--- assets_dev/js/index.js
+++ assets_dev/js/index.js
@@ -1,5 +1,6 @@
 const menu_hamburguer = document.querySelector('.icon-mobile');
 const menu = document.querySelector('.header-menu__mobile');
+const form = document.getElementById('formContato');
 
 menu_hamburguer.addEventListener('click', () => {
   const active = menu.classList.toggle('header-menu__mobile--active');
@@ -12,3 +13,20 @@ menu_hamburguer.addEventListener('click', () => {
   icon.classList.remove('fa-bars');
   icon.classList.add('fa-xmark');
 });
+
+async function sendEmail(event) {
+  event.preventDefault();
+  const formData = new FormData(this);
+  const data = JSON.stringify(Object.fromEntries(formData))
+  const email = await fetch("http://localhost:3000/send-email", {
+    body: data,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: "POST",
+  });
+  const resp = await email.json();
+  console.log(resp);
+}
+
+form.addEventListener('submit', sendEmail);
