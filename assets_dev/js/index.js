4545073284518e6d7a6e4d5ff619646d18e31fd1
Vanessa Santana <vanessa_santanatoledo@outlook.com>
2022-09-17 22:10:40 -0300

    refactor: melhorando o envio de email

--- assets_dev/js/index.js
+++ assets_dev/js/index.js
@@ -5,7 +5,8 @@ const toast_container =  document.querySelector('.contact-toast');
 const toast_icon =  document.querySelector('.toast-icon');
 const toast_message =  document.querySelector('.toast-message');
 const button_enviar_form = document.querySelector('.enviar-form');
-
+const BASE_URL = window.location.origin; 
+console.log(BASE_URL);
 menu_hamburguer.addEventListener('click', () => {
   const active = menu.classList.toggle('header-menu__mobile--active');
   const icon  = menu_hamburguer.querySelector('i');
@@ -31,7 +32,7 @@ function Toast (tipo, messagem) {
     toast_container.classList.remove(class_remove);
     toast_container.classList.add(tipo);
     toast_message.innerHTML = messagem;
-    toast_icon.innerHTML = danger ? '<i class="fa-solid fa-check"></i>' : '<i class="fa-solid fa-close"></i>';
+    toast_icon.innerHTML = danger ? '<i class="fa-solid fa-close"></i>' : '<i class="fa-solid fa-check"></i>';
     toast_container.style.display = 'flex';
     removeToast();
   }
@@ -39,9 +40,11 @@ function Toast (tipo, messagem) {
 
 async function sendEmail(event) {
   event.preventDefault();
+  button_enviar_form.innerHTML = '<img src="./assets/img/load.svg" alt="Carregando...">';
+  button_enviar_form.disabled = true;
   const formData = new FormData(this);
   const data = JSON.stringify(Object.fromEntries(formData))
-  const email = await fetch("http://localhost:3000/send-email", {
+  const email = await fetch(`${BASE_URL}/send-email`, {
     body: data,
     headers: {
       'Content-Type': 'application/json'
@@ -49,6 +52,8 @@ async function sendEmail(event) {
     method: "POST",
   });
   const resp = await email.json();
+  button_enviar_form.innerHTML = 'Enviar';
+  button_enviar_form.disabled = false;
   Toast(resp.tipo, resp.mensagem);
 }
 
